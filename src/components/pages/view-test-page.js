9824e6ad68c55b65cab237731a88a62963b0f075
Cody Johnson <ctj@math.cmu.edu>
2017-08-27 20:37:23 -0400

    started view test page draggable and droppable

--- src/components/pages/view-test-page.js
+++ src/components/pages/view-test-page.js
@@ -1,22 +1,94 @@
-import React from "react";
+import React, {Component} from "react";
+import {SortableContainer, SortableElement, SortableHandle, arrayMove} from 'react-sortable-hoc';
 import { Row, Col } from "react-materialize";
 import {  } from "../utilities";
 
+function removeProblem(key, removeElem) {
+  $('#problem' + key).fadeOut(300, function(){ $(this).remove();});
+  removeElem(key);
+  console.log(key);
+}
+
+const DragHandle = SortableHandle(() => <a href className="grey-text text-darken-1"><i className="fa fa-bars" aria-hidden="true"></i></a>); // This can be any component you want
+
+const SortableProblem = SortableElement(({ problem, myKey, removeElem }) => (
+  <li id={"problem" + myKey} className="test-list-item">
+    <div className="katex-preview">
+      <Row>
+        <Col s={1}>
+          <DragHandle />
+        </Col>
+        <Col s={10}>
+          { problem }
+        </Col>
+        <Col s={1}>
+          <a className="grey-text text-darken-1 right" onClick={() => removeProblem(myKey, removeElem)}><i className="fa fa-times" aria-hidden="true"></i></a>
+        </Col>
+      </Row>
+    </div>
+  </li>
+));
+
+const SortableProblemList = SortableContainer(({ problems, removeElem }) => {
+  return (
+    <ol className="test-list">
+      {problems.map((problem, key) => (
+        <SortableProblem problem={problem} key={key} myKey={key} index={key} removeElem={removeElem} />
+      ))}
+    </ol>
+  );
+});
+
+function removeElementWithIndex(arr, index) {
+  for (var i = 0; i < arr.length; i++)
+    if (arr[i].index == index)
+      return arr.splice(i);
+  return arr;
+}
+
+class TestProblems extends Component {
+  state = {
+    problems: [],
+  };
+
+  onSortEnd = ({oldIndex, newIndex}) => {
+    let {problems} = this.state;
+
+    this.setState({
+      problems: arrayMove(problems, oldIndex, newIndex),
+    });
+  };
+
+  removeElem = (i) => {
+    let {problems} = this.state;
+
+    this.setState({
+      problems: removeElementWithIndex(problems, i),
+    });
+  }
+
+  addElem = (statement) => {
+    let {problems} = this.state;
+
+    problems.push(statement);
+
+    this.setState({
+      problems: problems,
+    });
+  }
+
+  render() {
+    let {problems} = this.state;
+
+    return <div><SortableProblemList problems={problems} removeElem={this.removeElem} onSortEnd={this.onSortEnd} useDragHandle={true} /><a onClick={() => this.addElem("hello")}>+</a></div>;
+  }
+}
+
 const ViewTestPage = () => (
   <Row className="container">
-    <h2 className="teal-text text-darken-4">CMIMC 2018 Power Round</h2>
-    <table className="test-table">
-      <tbody>
-        <tr>
-          <td valign="top">1</td>
-          <td>problem</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>problem</td>
-        </tr>
-      </tbody>
-    </table>
+    <h2 className="teal-text text-darken-4">CMIMC 2018 Geometry (Individuals)</h2>
+    <p>This test consists of 10 problems.</p>
+    <TestProblems />
   </Row>
 );
 
