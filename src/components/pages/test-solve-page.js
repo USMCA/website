98fd6ab34f3e16a1692839e96adaff61b232284e
Cody Johnson <ctj@math.cmu.edu>
2017-08-29 19:06:50 -0400

    add changes

--- src/components/pages/test-solve-page.js
+++ src/components/pages/test-solve-page.js
@@ -4,13 +4,14 @@ import { LoadMore } from "../utilities";
 
 const TestSolvePage = () => (
   <Row className="container">
-    <Col m={7} s={12} style={{marginTop: "36px"}}>
-      <p>No test selected.</p>
+    <Col m={7} s={12} style={{marginTop: "24px"}}>
+      <p>Select a test from the <span className="bold-text">Contests</span> section on the right.</p>
     </Col>
-    <Col m={5} s={12} style={{marginTop: "36px"}}>
+    <Col m={5} s={12} style={{marginTop: "20px"}}>
+      <h3>Requests</h3>
       <div className="notifications-container">
         <ul className="notifications-list">
-          <li className="transparent">No notifications found.</li>
+          <li className="transparent">No test solve requests found.</li>
           <li className="new-announcement">
             <a href="#"><span className="select-circle"></span></a>
             <span className="bold-text">CMIMC</span> requests <span className="bold-text">5</span> test solvers. Their deadline is <span className="bold-text">January 20th, 2018</span>. <a href className="underline-hover">Apply</a>
@@ -27,7 +28,20 @@ const TestSolvePage = () => (
         </ul>
       </div>
       <h3>Contests</h3>
-
+      <ul className="contests-list">
+        <li>CMIMC 2018 (deadline: <span className="bold-text">January 20th, 2018</span>)
+          <ul>
+            <li><a className="underline-hover teal-text text-darken-3">test 1</a> (solved: 1/10)</li>
+            <li><a className="underline-hover teal-text text-darken-3">test 2</a> (solved: 7/10)</li>
+          </ul>
+        </li>
+        <li>PUMaC 2018 (deadline: <span className="bold-text">January 20th, 2018</span>)
+          <ul>
+            <li><a className="underline-hover teal-text text-darken-3">test 1</a> (solved: 2/10)</li>
+            <li><a className="underline-hover teal-text text-darken-3">test 2</a> (solved: 4/10)</li>
+          </ul>
+        </li>
+      </ul>
     </Col>
   </Row>
 );
