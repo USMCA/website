08019bbe6632ba41023bac747a4cfe30de6eeb56
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2017-09-04 17:37:40 -0400

    complete view contest except for getting directors and secure members

--- src/components/pages/view-contest-page.js
+++ src/components/pages/view-contest-page.js
@@ -10,119 +10,123 @@ import CreateContestForm from "../forms/create-contest";
 import RequestTSForm from "../forms/request-test-solvers";
 import CreateTestForm from "../forms/create-test";
 
-const contestTabs = ({ name, date, locations, tests, czars, testSolvers }) => ({
-  "tests": {
-    title: () => "Tests",
-    view: () => (
-      <div className="round-container">
-        <Modal trigger={<Button className="teal darken-3" waves="light">Add</Button>}><CreateTestForm /></Modal>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Problems</th>
-              <th>Options</th>
-            </tr>
-          </thead> 
-          <tbody>
-            {
-              tests.map((test, key) => (
-                <tr key={key}>
-                  <td>{test.name}</td>
-                  <td>{test.problems}</td>
-                  <td><Link to={ `/view-test/${test._id}` } className="teal-text text-darken-3 underline-hover">Manage</Link></td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </Table>
-      </div>
-    )
-  },
-  "czars": {
-    title: () => "Czars",
-    view: () => (
-      <div className="round-container">
-        <Button className="teal darken-3" waves="light">Invite</Button>
-        <h3>Czars</h3>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th className="center-align">Remove</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {
-              czars.map((czar, key) => (
-                <tr key={key}>
-                  <td>{czar}</td>
-                  <td>ctj@math.cmu.edu</td>
-                  <td className="center-align"><a className="black-text"><i className="fa fa-times" aria-hidden="true" /></a></td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </Table><br />
-        <RightButtonPanel>
-          <a className="btn teal darken-3 waves-effect waves-light">Leave as czar</a>
-        </RightButtonPanel>
-      </div>
-    )
- },
-  "test-solvers": {
-    title: () => "Test Solvers",
-    view: () => (
-      <div className="round-container">
-        <Button className="teal darken-3" waves="light">Invite</Button> <Modal trigger={<Button className="teal darken-3" waves="light">Request</Button>}><RequestTSForm /></Modal>
-        <h3>Test Solvers</h3>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th className="center-align">Remove</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {
-              testSolvers.map((czar, key) => (
-                <tr key={key}>
-                  <td>{czar}</td>
-                  <td>ctj@math.cmu.edu</td>
-                  <td className="center-align"><a className="black-text"><i className="fa fa-times" aria-hidden="true"></i></a></td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </Table>
-      </div>
-    )
-  }
-});
-
-const tests = [
-  {name: "Algebra (Individuals)", problems: 10},
-  {name: "Combinatorics (Individuals)", problems: 10},
-  {name: "Computer Science (Individuals)", problems: 10},
-  {name: "Geometry (Individuals)", problems: 10},
-  {name: "Number Theory (Individuals)", problems: 10},
-  {name: "Team", problems: 10},
-  {name: "Algebra (Individuals)", problems: 1}
-], date="January 28, 2018", czars = ["Tai", "Ailee"], 
-  testSolvers=["Ailee"], locations=["asdf"], name="CMIMC Ailee";
-
 class ContestPreviewDumb extends React.Component {
+  contestTabs = {
+    "tests": {
+      title: () => "Tests",
+      view: ({ data: { content } }) => {
+        if (!content) return <div />;
+        return (
+          <div className="round-container">
+            <Modal trigger={<Button className="teal darken-3" waves="light">Create</Button>}><CreateTestForm /></Modal>
+            { (content.tests.length > 0) ? (
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Problems</th>
+                      <th>Options</th>
+                    </tr>
+                  </thead> 
+                  <tbody>
+                  {
+                    content.tests.map((test, key) => (
+                      <tr key={key}>
+                        <td>{test.name}</td>
+                        <td>{test.problems}</td>
+                        <td><Link to={ `/view-test/${test._id}` } className="teal-text text-darken-3 underline-hover">Manage</Link></td>
+                      </tr>
+                    ))
+                  }
+                  </tbody>
+                </Table>
+              ) : ( <p>No tests created yet.</p> )
+            }
+          </div>
+        );
+      }
+    },
+    "czars": {
+      title: () => "Czars",
+      view: ({ data: { content } }) => {
+        if (!content) return <div />;
+        return (
+          <div className="round-container">
+            <Button className="teal darken-3" waves="light">Invite</Button>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th className="center-align">Remove</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {
+                  content.czars.map((czar, key) => (
+                    <tr key={key}>
+                      <td>{czar}</td>
+                      <td>ctj@math.cmu.edu</td>
+                      <td className="center-align"><a className="black-text"><i className="fa fa-times" aria-hidden="true" /></a></td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </Table><br />
+          </div>
+        );
+      }
+   },
+    "test-solvers": {
+      title: () => "Test Solvers",
+      view: ({ data: { content } }) => {
+        if (!content) return <div />;
+        return (
+          <div className="round-container">
+            <Button className="teal darken-3" waves="light">Invite</Button> <Modal trigger={<Button className="teal darken-3" waves="light">Request</Button>}><RequestTSForm /></Modal>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th className="center-align">Remove</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {
+                  content.test_solvers.map((czar, key) => (
+                    <tr key={key}>
+                      <td>{czar}</td>
+                      <td>ctj@math.cmu.edu</td>
+                      <td className="center-align"><a className="black-text"><i className="fa fa-times" aria-hidden="true"></i></a></td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </Table>
+          </div>
+        );
+      }
+    }
+  };
 
   render() {
-    console.log(this.props.data);
+    const { data } = this.props,
+          contest = data.content;
+    if (!contest) return <div />;
+
+    const childProps = {
+            "tests": { data },
+            "czars": { data },
+            "test-solvers": { data }
+          };
+
     return (
       <Col s={12}>
-        <h2 className="teal-text text-darken-3">{name}</h2>
-        <VerticalNav tabs={ contestTabs({ name, date, locations, tests, czars, testSolvers })} active="tests" />
+        <h2 className="teal-text text-darken-3">{contest.name}</h2>
+        <VerticalNav tabs={ this.contestTabs } childProps={ childProps } active="tests" />
       </Col>
     );
   }
